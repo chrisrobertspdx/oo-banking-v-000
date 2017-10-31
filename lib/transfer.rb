333fa6c011e7613d36f20d7332b75c8f845da526
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-31 17:15:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,8 @@ class Transfer
       self.status = "complete"
       self
     else
-      "Transaction rejected. Please check your account balance."
       self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
